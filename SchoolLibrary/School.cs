89267f812bf7afa66a331f83eb43f9d3059f0905
John.michael <John.michael@studentambassadors.com>
2021-05-18 21:00:57 -0500

    added classes school class properties ,acess mofifiers and two new folders highschool and elementary

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+       public string _twitterAdress { get; set; }
+      public string SchoolName { get; set; }
+      public string SchoolAdress { get; set; }
+      public string City{ get; set; }
+      public  string Principal { get; set; }
+      private  string PhoneNumber { get; set; }
+      public string TwitterAdress
+        {
+            //make sure the twitter address starts with a @
+            get { return _twitterAdress; }
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    throw new Exception("twitter address formating should start with @");
+                }
+            }
+        }
+    }
+}
